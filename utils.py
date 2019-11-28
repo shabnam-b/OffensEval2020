4792543c19fe1f96fd9603d742ee8296efa98103
yilunzhu <allen.yl.zhu@gmail.com>
2019-11-28 11:26:28 -0500

    utils

--- utils.py
+++ utils.py
@@ -8,36 +8,66 @@ import tensorflow as tf
 import emoji
 from ekphrasis.classes.preprocessor import TextPreProcessor
 import wordsegment
+import nltk
+
+
+def load_vec(path):
+    embeddings_index = dict()
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    return embeddings_index
+
+
+def tokenize(sents, max_seq_len):
+    embeddings_index = load_vec("glove.6B/glove.6B.300d.txt")
+    embeddings = []
+    for sent in sents:
+        sent_embed = []
+        sent_tokenized = nltk.word_tokenize(sent)
+        for word in sent_tokenized:
+            if word not in embeddings_index.keys():
+                sent_embed.append([0.0001] * 300)
+            else:
+                sent_embed.append(embeddings_index[word])
+        for _ in range(len(sent_tokenized), max_seq_len):
+            sent_embed.append(([0.0]*300))
+        embeddings.append(sent_embed)
+    return np.asanyarray(embeddings)
 
 
 def tensorize(input_tensor, type):
     return tf.convert_to_tensor(input_tensor, type)
 
 
-def tokenize(sents, max_seq_len):
-    """Tokenize the data sent by sent into Bert sub-tokens. Append input masks"""
-    bert_tokenizer = tokenization.FullTokenizer(vocab_file="cased_L-12_H-768_A-12/vocab.txt", do_lower_case=True)
-    input_ids = []
-    input_mask = []
-    examples = []
-    for sent in sents:
-        """Error may be occured due to non-unicode text (emoji)"""
-        sent_tokenized = bert_tokenizer.tokenize(sent)
-        example = ["[CLS]"] + sent_tokenized + ["[SEP]"]
-        sent_input_ids = bert_tokenizer.convert_tokens_to_ids(example)
-        sent_input_mask = [1] * len(example)
-        for _ in range(len(example), max_seq_len):
-            sent_input_ids.append(0)
-            sent_input_mask.append(0)
-        input_ids.append(sent_input_ids[:max_seq_len])
-        input_mask.append(sent_input_mask[:max_seq_len])
-        examples.append(example[:max_seq_len])
-        assert len(sent_input_ids) == len(sent_input_mask)
-
-    input_ids = np.array(input_ids)
-    input_mask = np.array(input_mask)
-
-    return tensorize(input_ids, tf.int32), tensorize(input_mask, tf.int32), examples
+# def tokenize(sents, max_seq_len):
+#     """Tokenize the data sent by sent into Bert sub-tokens. Append input masks"""
+#     # bert_tokenizer = tokenization.FullTokenizer(vocab_file="cased_L-12_H-768_A-12/vocab.txt", do_lower_case=True)
+#     input_ids = []
+#     input_mask = []
+#     examples = []
+#     for sent in sents:
+#         """Error may be occured due to non-unicode text (emoji)"""
+#         sent_tokenized = bert_tokenizer.tokenize(sent)
+#         example = ["[CLS]"] + sent_tokenized + ["[SEP]"]
+#         sent_input_ids = bert_tokenizer.convert_tokens_to_ids(example)
+#         sent_input_mask = [1] * len(example)
+#         for _ in range(len(example), max_seq_len):
+#             sent_input_ids.append(0)
+#             sent_input_mask.append(0)
+#         input_ids.append(sent_input_ids[:max_seq_len])
+#         input_mask.append(sent_input_mask[:max_seq_len])
+#         examples.append(example[:max_seq_len])
+#         assert len(sent_input_ids) == len(sent_input_mask)
+#
+#     input_ids = np.array(input_ids)
+#     input_mask = np.array(input_mask)
+#
+#     return tensorize(input_ids, tf.int32), tensorize(input_mask, tf.int32), examples
 
 
 def bert_transformer(input_ids, input_mask, bert_config, config):
@@ -115,10 +145,10 @@ config = {'bert_config': 'cased_L-12_H-768_A-12/bert_config.json',
 sents, label_a, label_b = load_train_data(config)
 
 sents = clean_tweets(sents)
-input_ids, input_mask, input_tokens = tokenize(sents, max_seq_len=64)
+embeddings = tokenize(sents, max_seq_len=64)
 
-with tf.variable_scope('bert_embedding'):
-    bert_config = modelling.BertConfig.from_json_file(config['vocab_file'])
-    bert_encoder_layer = bert_transformer(input_ids, input_mask, bert_config, config)
-
-print("Done bert.")
+# with tf.variable_scope('bert_embedding'):
+#     bert_config = modelling.BertConfig.from_json_file(config['vocab_file'])
+#     bert_encoder_layer = bert_transformer(input_ids, input_mask, bert_config, config)
+#
+# print("Done bert.")
